cdbb21dd9555a0e3ee8a8b8da88e114f497c620d
Justin-dev-techmoz <justindevlab@gmail.com>
2025-06-28 23:59:36 +0200

    Temporarily disable all Prisma-dependent modules for deployment

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,32 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
-import { UserModule } from './user.module';
-import { PrismaModule } from './prisma.module';
-import { CategoryModule } from '../category/category.module';
-import { PublisherModule } from '../publisher/publisher.module';
-import { BookModule } from '../book/book.module';
-import { StatisticsModule } from '../statistics/statistics.module';
-import { ReservationModule } from '../reservation/reservation.module';
-import { LoanModule } from '../loan/loan.module';
-import { BookItemModule } from '../book-item/book-item.module';
-import { RequestModule } from '../request/request.module';
-import { EventModule } from '../event/event.module';
+// import { AuthModule } from '../auth/auth.module';
+// import { UserModule } from './user.module';
+// import { PrismaModule } from './prisma.module';
+// import { CategoryModule } from '../category/category.module';
+// import { PublisherModule } from '../publisher/publisher.module';
+// import { BookModule } from '../book/book.module';
+// import { StatisticsModule } from '../statistics/statistics.module';
+// import { ReservationModule } from '../reservation/reservation.module';
+// import { LoanModule } from '../loan/loan.module';
+// import { BookItemModule } from '../book-item/book-item.module';
+// import { RequestModule } from '../request/request.module';
+// import { EventModule } from '../event/event.module';
 import { HealthModule } from '../health/health.module';
 
 @Module({
   imports: [
-    AuthModule,
-    UserModule,
-    PrismaModule,
-    CategoryModule,
-    PublisherModule,
-    BookModule,
-    StatisticsModule,
-    ReservationModule,
-    LoanModule,
-    BookItemModule,
-    RequestModule,
-    EventModule,
+    // AuthModule,
+    // UserModule,
+    // PrismaModule,
+    // CategoryModule,
+    // PublisherModule,
+    // BookModule,
+    // StatisticsModule,
+    // ReservationModule,
+    // LoanModule,
+    // BookItemModule,
+    // RequestModule,
+    // EventModule,
     HealthModule,
   ],
   controllers: [],
